d337cdfd7f1385b654487d5b3ef957b44687348a
ivan <ivansos7890@gmail.com>
2026-06-22 16:02:40 +0200

    Finished TransformModule

--- src/Core/ModuleCore.cpp
+++ src/Core/ModuleCore.cpp
@@ -1,97 +1,98 @@
-#include "ModuleCore.hpp"
-ModuleCore::ModuleCore() {}
-ModuleCore::ModuleCore(uint128_t UUID)
-{   
-    string cmd = "SET_STATE";
-    this->command.push_back(cmd);
-    this->type = "ModulCore";
-    this->UUID = UUID;
-    this->callback = ([&]( vector<float>) {});
-    this->SetOnCommand(cmd, this->callback );
-}
+    #include "ModuleCore.hpp"
 
-void ModuleCore::SetOnCommand(string command, function<void(vector<float>)> callback)
-{
-    auto it = this->commandCallBack.find(command);
-    if (it == this->commandCallBack.end())
-        this->commandCallBack.insert( {command, vector < function < void (vector<float> ) > >{} } );
-    commandCallBack[command].push_back(callback);
-}
+    ModuleCore::ModuleCore() {}
+    ModuleCore::ModuleCore(uint128_t UUID)
+    {   
+        string cmd = "SET_STATE";
+        this->command.push_back(cmd);
+        this->type = "ModulCore";
+        this->UUID = UUID;
+        this->callback = ([&]( vector<float>) {});
+        this->SetOnCommand(cmd, this->callback );
+    }
 
-void ModuleCore::OnCommand(string command, vector<float> data)
-{
-    auto it = this->commandCallBack.find(command);
-    if (it != this->commandCallBack.end())
-        for (auto callback_temp : it->second)
-            callback_temp(data);
-    else
-        cout << this->UUID << " has no member " << endl;
-}
+    void ModuleCore::SetOnCommand(string command, function<void(vector<float>)> callback)
+    {
+        auto it = this->commandCallBack.find(command);
+        if (it == this->commandCallBack.end())
+            this->commandCallBack.insert( {command, vector < function < void (vector<float> ) > >{} } );
+        commandCallBack[command].push_back(callback);
+    }
 
-void ModuleCore::SetOutputFn( function<void (pair<string, vector<float> >) > outputFn )
-{
-    this->outputFn = outputFn;
-}
+    void ModuleCore::OnCommand(string command, vector<float> data)
+    {
+        auto it = this->commandCallBack.find(command);
+        if (it != this->commandCallBack.end())
+            for (auto callback_temp : it->second)
+                callback_temp(data);
+        else
+            cout << this->UUID << " has no member " << endl;
+    }
 
-uint128_t ModuleCore::GetUUID()
-{
-    return this->UUID;
-}
+    void ModuleCore::SetOutputFn( function<void (pair<string, vector<float> >) > outputFn )
+    {
+        this->outputFn = outputFn;
+    }
 
-void add(vector<float> tab)
-{
-    float sum = 0;
-    for (auto it : tab)
-        sum += it;
-    cout << "Sum = " << sum << endl;
-}
+    uint128_t ModuleCore::GetUUID()
+    {
+        return this->UUID;
+    }
 
-void sub(vector<float> tab)
-{
-    float sub = 0;
-    for (auto it : tab)
-        sub -= it;
-    cout << "Sub = " << sub << endl;
-}
+    void add(vector<float> tab)
+    {
+        float sum = 0;
+        for (auto it : tab)
+            sum += it;
+        cout << "Sum = " << sum << endl;
+    }
 
-void mul(vector<float> tab)
-{
-    float mul = 1;
-    for (auto it : tab)
-        mul *= it;
-    cout << "Mul = " << mul << endl;
-}
+    void sub(vector<float> tab)
+    {
+        float sub = 0;
+        for (auto it : tab)
+            sub -= it;
+        cout << "Sub = " << sub << endl;
+    }
 
-int main()
-{ 
+    void mul(vector<float> tab)
+    {
+        float mul = 1;
+        for (auto it : tab)
+            mul *= it;
+        cout << "Mul = " << mul << endl;
+    }
 
-    ModuleCore mod = ModuleCore( 1234 );
-     ModuleCore mod1 = ModuleCore( 1234 );
-     mod.SetOnCommand("TEST", [&]( vector<float> v) {
-         cout << v.size() << endl;
-     });
-         mod.SetOnCommand("TEST", [&]( ::vector<float> v) {
-         cout << "test2 " << v.size() << endl;
-     });
-             mod.SetOnCommand("COMMAND", [&]( ::vector<float> v) {
-         cout << "command " << v.size() << endl;
-     });
-     mod.OnCommand( "TEST", vector<float> { 1.0, 2.0, 3.0 });
-     mod.OnCommand( "COMMAND", vector<float> { 1.0, 2.0, 3.0 });
+    int main()
+    { 
 
-     mod.SetOutputFn( [&](pair<string, vector<float>> message){ 
-         mod1.OnCommand( message.first, message.second );
-     } );
+        ModuleCore mod = ModuleCore( 1234 );
+        ModuleCore mod1 = ModuleCore( 1234 );
+        mod.SetOnCommand("TEST", [&]( vector<float> v) {
+            cout << v.size() << endl;
+        });
+            mod.SetOnCommand("TEST", [&]( ::vector<float> v) {
+            cout << "test2 " << v.size() << endl;
+        });
+                mod.SetOnCommand("COMMAND", [&]( ::vector<float> v) {
+            cout << "command " << v.size() << endl;
+        });
+        mod.OnCommand( "TEST", vector<float> { 1.0, 2.0, 3.0 });
+        mod.OnCommand( "COMMAND", vector<float> { 1.0, 2.0, 3.0 });
 
-     mod1.SetOutputFn( [&](pair<string, vector<float>> message){ 
-         mod.OnCommand( message.first, message.second );
-     } );
-     vector <float> tab{1.,2.,3.};
+        mod.SetOutputFn( [&](pair<string, vector<float>> message){ 
+            mod1.OnCommand( message.first, message.second );
+        } );
 
-     
-    mod1.outputFn(pair<string , vector<float>> ("TEST" , tab) );
-    
-    return 0;
-}
-    
-    
+        mod1.SetOutputFn( [&](pair<string, vector<float>> message){ 
+            mod.OnCommand( message.first, message.second );
+        } );
+        vector <float> tab{1.,2.,3.};
+
+        
+        mod1.outputFn(pair<string , vector<float>> ("TEST" , tab) );
+        
+        return 0;
+    }
+        
+        
--- src/TransformModule.hpp
+++ src/TransformModule.hpp
@@ -1,11 +1,27 @@
 #ifndef TRANSFORMMODULE_HHP
 #define TRANSFORMMODULE_HHP
+
+#define TRANSLATION_SIZE 3
+#define ROTATION_SIZE 4
+#define SCALE_SIZE 3
+
 #include "Core/ModuleCore.hpp"
+#include <array>
+#include <tuple>
 
 class TransformModule : public ModuleCore
 {
+    protected:
+        std::map<std::string, std::vector <float>> commands;
+        std::array<float, TRANSLATION_SIZE> translation {};
+        std::array<float, ROTATION_SIZE> rotation {};
+        std::array<float, SCALE_SIZE> scale {};
     public:
-        TransformModule();
+        TransformModule(uint128_t UUID);
+        void updateTransform(vector<float> transform, bool sync);
+        std::tuple<std::array<float, TRANSLATION_SIZE>, std::array<float, ROTATION_SIZE>, std::array<float, SCALE_SIZE>> getTransform();
+        map<string, vector<float>> getState();
+        void setState(map<string, vector<float>> state);
 };
   
 #endif
