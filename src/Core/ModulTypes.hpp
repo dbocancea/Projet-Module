d7b3c14b1a2aca4e7b5c37522169d1ca33bb8608
Bocancea Damian <damian.bocancea29@gmail.com>
2026-06-22 13:43:43 +0200

    I added all types in ModulTypes

--- src/Core/ModulTypes.hpp
+++ src/Core/ModulTypes.hpp
@@ -2,12 +2,37 @@
 #define MODULTYPES_HPP
 #include "ModuleCore.hpp"
 #include "ModuleRegistry.hpp"
+#include "../CameraModule.hpp"
+#include "../FileModule.hpp"
+#include "../GLTFModule.hpp"
+#include "../LineModule.hpp"
+#include "../PointsModule.hpp"
+#include "../PrimitiveModule.hpp"
+#include "../ScalarModule.hpp"
+#include "../TextLogModule.hpp"
+#include "../TextModule.hpp"
+#include "../TransformModule.hpp"
+#include "../TriggerModule.hpp"
+#include "../Vector3Module.hpp"
 
 using FnModulTypes = function<ModuleCore*()>;
 
 const map<string ,FnModulTypes > ModulTypes = {
     {"ModuleCore" , [](){ return new ModuleCore(); }  },
-    {"ModuleRegestry" , [](){ return new ModuleRegistry(); }  }
+    {"ModuleRegestry" , [](){ return new ModuleRegistry(); }  },
+    {"CameraModule" , [](){ return new CameraModule(); }  },
+    {"FileModule" , [](){ return new FileModule(); }  },
+    {"GLTFModule" , [](){ return new GLTFModule(); }  },
+    {"LineModule" , [](){ return new LineModule(); }  },
+    {"PointsModule" , [](){ return new PointsModule(); }  },
+    {"PrimitiveModule" , [](){ return new PrimitiveModule(); }  },
+    {"ScalarModule" , [](){ return new ScalarModule(); }  },
+    {"TextLogModule" , [](){ return new TextLogModule(); }  },
+    {"TextModule" , [](){ return new TextModule(); }  }
+    {"TransformModule" , [](){ return new TransformModule(); }  },
+    {"TriggerModule" , [](){ return new TriggerModule(); }  },
+    {"Vector3Module" , [](){ return new Vector3Module(); }  },
+
 };
 
 
