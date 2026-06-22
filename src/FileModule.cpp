21311bfb010b267e830b19d205ab756e49d1be50
Bocancea Damian <damian.bocancea29@gmail.com>
2026-06-22 17:55:15 +0200

    I changed FileModule

--- src/FileModule.cpp
+++ src/FileModule.cpp
@@ -3,4 +3,21 @@
 FileModule::FileModule()
 {
     this->type = "FileModule";
-};
+}
+
+FileModule::FileModule( uint128_t UUID ) : ModuleCore(UUID)
+{
+    this->type = "FileModule";
+    string cmd = "UPDATE_FILE";
+    this->command.push_back(cmd);
+    this->SetOnCommand(cmd , [&](vector<float> file) {this->UpdateFile(file, false);}  );
+}
+
+void FileModule::UpdateFile(vector<float> file , bool sync)
+{
+    this->file = file;
+    this->OnChange("UPDATE_FILE" , this->file );
+
+    if( sync )
+        this->outputFn(pair<string, vector<float> >{"UPDATE_FILE" , file});
+}
--- src/FileModule.hpp
+++ src/FileModule.hpp
@@ -3,9 +3,13 @@
 #include "Core/ModuleCore.hpp"
 
 class FileModule : public ModuleCore
-{
+{   
+    protected:
+        vector<float> file;
     public:
         FileModule();
+        FileModule(uint128_t UUID);
+        void UpdateFile(vector<float> file , bool sync);
 };  
 
 #endif
