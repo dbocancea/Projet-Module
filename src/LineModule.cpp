c64273491a633ad7feb219af46053bc46ea6f95b
Shchaslyvyi Ernest <ernipro26@gmail.com>
2026-06-23 14:12:42 +0200

    Made a part of LineModule

--- src/LineModule.cpp
+++ src/LineModule.cpp
@@ -1,6 +1,22 @@
 #include "LineModule.hpp"
 
-LineModule::LineModule()
+
+LineModule::LineModule() {};
+LineModule::LineModule(uint128_t)
 {
     this->type = "LineModule";
+
+    this->origin = {0.,0.,0.};
+    this->end = {0.,0.,0.};
+
+    string updLine = "UPDATE_LINE";
+    this->command.push_back(updLine);
+
+    this->SetOnCommand(updLine, [&](vector<float> data) {this->updateLine(data);});
+ 
+}
+
+void LineModule::updateLine(std::vector<float> line) // not possible to do for now: JSON (not vect) needed to work properly
+{
+    cout << "LineModule - updateLine" << endl;
 }
--- src/LineModule.hpp
+++ src/LineModule.hpp
@@ -4,8 +4,16 @@
 
 class LineModule : public ModuleCore
 {
+    private:    
+        vector<float> origin;
+        vector<float> end;
+
     public:
         LineModule();
+        LineModule(uint128_t UUID);
+        void updateLine(vector<float> line); 
+
+
 };
 
 #endif
