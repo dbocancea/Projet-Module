db402e2ae39cb32337670290daa1aec1bc7a740a
ivan <ivansos7890@gmail.com>
2026-06-23 14:37:09 +0200

    Begining of PointsModule with the completed header

--- src/PointsModule.cpp
+++ src/PointsModule.cpp
@@ -4,3 +4,12 @@ PointsModule::PointsModule()
 {
     this->type = "PointsModule";
 }
+
+PointsModule::PointsModule(uint128_t UUID) : ModuleCore(UUID)
+{
+    this->type = "PointsModule";
+    this->SetOnCommand("ADD_POINTS", [this](const vector<float>& points)
+    {
+        this->addPoints(points, false);
+    });
+}
