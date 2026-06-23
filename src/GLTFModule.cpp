4b530c1cab90f71a331857e2665adde0f8733fcd
Bocancea Damian <damian.bocancea29@gmail.com>
2026-06-23 16:37:09 +0200

    I changed GLTFModule

--- src/GLTFModule.cpp
+++ src/GLTFModule.cpp
@@ -4,3 +4,19 @@ GLTFModule::GLTFModule()
 {
     this->type = "GLTFModule";
 };
+
+GLTFModule::GLTFModule( uint128_t UUID )
+{
+    this->UUID = UUID;
+    this->command.push_back("SET_SCENE_GRAPH");
+    this->command.push_back("SET_NODES");
+    this->command.push_back("UPDATE_NODES");
+    this->command.push_back("CLEAR");
+
+    this->type = "GLTFModule";
+
+    this->SetOnCommand("SET_NODES" , [&]( vector<float>) { this. } )
+    
+};
+
+
--- src/GLTFModule.hpp
+++ src/GLTFModule.hpp
@@ -1,11 +1,19 @@
 #ifndef GLTFMODULE_HPP
 #define GLTFMODULE_HPP
-#include "Core/ModuleCore.hpp"
+#include "FileModule.hpp"
+#include "SceneGraph.hpp"
 
-class GLTFModule : public  ModuleCore
-{
+
+
+class GLTFModule : public  FileModule
+{   
+    protected:
+        const uint128_t root_UUID = 0;
+        SceneGraph sceneGraph;
     public:
         GLTFModule();
+        GLTFModule(uint128_t UUID);
+        void SetNodes();
 };
   
 #endif
