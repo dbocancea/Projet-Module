ddd2bb77a865ef095f81af9948500ab942b492fd
dbocancea <damian.bocancea29@gmail.com>
2026-06-24 14:36:10 +0200

    I made GLTFModule

--- src/GLTFModule.cpp
+++ src/GLTFModule.cpp
@@ -15,8 +15,33 @@ GLTFModule::GLTFModule( uint128_t UUID )
 
     this->type = "GLTFModule";
 
-    this->SetOnCommand("SET_NODES" , [&]( vector<float>) { this. } )
+    this->SetOnCommand("SET_NODES" , [&]( vector<Nodes> node) { this->SetNodes(node); } );
     
+    this->SetOnCommand("SET_NODES" , [&]( vector<Nodes> node) { this->SetNodes(node); } );
 };
 
+void GLTFModule::SetNodes(vector<Nodes> nodes)
+{
+    vector<uint128_t> nodesUUID;
+    for( auto node : nodes )
+    {
+        sceneGraph.AddNode(node);
+        nodesUUID.push_back(node.UUID);
+    }
+    vector<Nodes> nodes = sceneGraph.NodesData(nodesUUID);
+    this->OnChange("SET_NODES" , nodes);
+}
 
+void GLTFModule::UpdateNodes(vector<Nodes> nodes)
+{
+    vector<uint128_t> nodesUUID;
+    for( auto node : nodes )
+    {
+        sceneGraph.UpdateNode(node);
+        nodesUUID.push_back(node.UUID);
+    }
+    vector<Nodes> nodes = sceneGraph.NodesData(nodesUUID);
+    this->OnChange("UPDATE_NODES" , nodes);
+
+
+}
--- src/GLTFModule.hpp
+++ src/GLTFModule.hpp
@@ -5,15 +5,17 @@
 
 
 
-class GLTFModule : public  FileModule
+class GLTFModule : public  ModuleCore<vector<Nodes>>
 {   
     protected:
+        FileModule fileHandler;
         const uint128_t root_UUID = 0;
         SceneGraph sceneGraph;
     public:
         GLTFModule();
         GLTFModule(uint128_t UUID);
-        void SetNodes();
+        void SetNodes(vector<Nodes> nodes );
+        void UpdateNodes(vector<Nodes> nodes );
 };
   
 #endif
