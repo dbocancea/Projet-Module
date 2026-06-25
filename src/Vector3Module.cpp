85d7f7fbb84f29734c52ef38365134b63be75f77
dbocancea <damian.bocancea29@gmail.com>
2026-06-25 15:14:41 +0200

    I added VectorModule

--- src/Vector3Module.cpp
+++ src/Vector3Module.cpp
@@ -4,3 +4,39 @@ Vector3Module::Vector3Module()
 {
     this->type = "Vector3Module";
 }
+
+Vector3Module::Vector3Module(uint128_t UUID) : ModuleCore(UUID)
+{
+    cout << "Vector3Module - constructor" << endl;
+    this->type = "Vector3Module";
+    this->command.push_back("UPDATE_VECTOR");
+
+    this->SetOnCommand("UPDATE_VECTOR", [&](array<int, 3> vector)
+                       { this->UpdateVector(vector, 0); });
+}
+
+void Vector3Module::UpdateVector(array<int, 3> vector, bool sync)
+{
+    cout << "Vector3Module - updateVector" << endl;
+
+    for (int i = 0; i < vector.size(); i++)
+        this->vector[i] = vector[i];
+    this->OnChange("UPDATE_VECTOR", this->vector);
+
+    if (sync)
+        this->outputFn({"UPDATE_VECTOR", vector});
+}
+
+array<int, 3> Vector3Module::GetVector()
+{
+    return this->vector;
+}
+
+array<int, 3> Vector3Module::GetState()
+{
+    return this->vector;
+}
+void Vector3Module::SetState( array<int, 3> vector)
+{
+    this->UpdateVector(vector , 0);
+}
