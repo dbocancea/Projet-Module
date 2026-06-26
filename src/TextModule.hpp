af338b787d6f3e0c1bb68af1ecfaa553c24f45eb
dbocancea <damian.bocancea29@gmail.com>
2026-06-26 13:53:29 +0200

    I added TextModule

--- src/TextModule.hpp
+++ src/TextModule.hpp
@@ -2,10 +2,17 @@
 #define TEXTMODULE_HPP
 #include "Core/ModuleCore.hpp"
 
-class TextModule : public ModuleCore
+class TextModule : public ModuleCore<string>
 {
+    protected:
+        string text;
     public:
         TextModule();
+        TextModule(uint128_t UUID);
+        void UpdateText(string text; bool sync);
+        string GetText();
+        string GetState();
+        void SetState(string state);
 };
   
 #endif
