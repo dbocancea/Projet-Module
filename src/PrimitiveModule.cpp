2be6857deb58fc19895b3eefc843d98765d70bd7
ivan <ivan.sosonnyi@etun.unistra.fr>
2026-07-02 09:20:11 +0200

    Nothing

--- src/PrimitiveModule.cpp
+++ src/PrimitiveModule.cpp
@@ -1,6 +1,6 @@
 #include "PrimitiveModule.hpp"
 
-PrimitiveModule::PrimitiveModule()
+PrimitiveModule::PrimitiveModule() : TransformModule(0)
 {
     this->type = "PrimitiveModule";
 }
@@ -8,39 +8,46 @@ PrimitiveModule::PrimitiveModule()
 PrimitiveModule::PrimitiveModule(uint128_t UUID) : TransformModule(UUID)
 {
     this->type = "PrimitiveModule";
-    this->SetOnCommand("UPDATE_PRIMITIVE", [this](this->primitive, false)
+    this->SetOnCommand("UPDATE_PRIMITIVE", [this](string primitive)
     {
-        this->updatePrimitive(this->primitive, false);
+        this->updatePrimitive(primitive);
     });
+}
 
-    string PrimitiveModule::getPrimitive()
+string PrimitiveModule::getPrimitive()
+{
+    return this->primitive;
+}
+
+vector<string> PrimitiveModule::getPrimitiveTypes()
+{
+    return this->primitiveTypes;
+}
+
+void PrimitiveModule::updatePrimitive(string primitive, bool sync)
+{
+    this->primitive = primitive;
+
+    this->OnChange("UPDATE_PRIMITIVE", this->primitive);
+
+    if(sync)
+        cout << "UPDATE_PRIMITIVE " << this->primitive << endl;
+}
+
+map<string, map<string, vector<float>>> PrimitiveModule::getState()
+{
+    map<string, map<string, vector<float>>> res {};
+    auto transState = this->TransformModule::getState();
+    res[this->TransformModule::getState()] = this->primitive;
+    return res;
+}
+
+void PrimitiveModule::setState(map<map<string, vector<float>>, string> state)
+{
+    if(!state.empty())
     {
-        return this->primitive;
-    }
-
-    vector<string> PrimitiveModule::getPrimitiveTypes()
-    {
-        return this->primitiveTypes;
-    }
-
-    void PrimitiveModule::updatePrimitive(string primitive, bool sync)
-    {
-        this->primitive = primitive;
-
-        this->OnChange("UPDATE_PRIMITIVE", primitive);
-
-        if(sync)
-            cout << "UPDATE_PRIMITIVE " << this->primitive << endl;
-    }
-
-    map<map<string, vector<float>>, string> PrimitiveModule::getState()
-    {
-        return {this->getState(), this->primitive};
-    }
-
-    void PrimitiveModule::setState(map<map<string, vector<float>>, string> state)
-    {
-        this->setState(state);
-        this->updatePrimitive(state->second);
+        auto it = state.begin();
+        this->TransformModule::setState(it->first);
+        this->updatePrimitive(it->second);
     }
 }
