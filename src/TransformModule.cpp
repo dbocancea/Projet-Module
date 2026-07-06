e75ef6cf1ef28f54b107b245f2603125982e7220
ivan <ivansos7890@gmail.com>
2026-07-06 18:17:29 +0200

    onUpdate

--- src/TransformModule.cpp
+++ src/TransformModule.cpp
@@ -12,37 +12,61 @@ TransformModule::TransformModule( uint128_t UUID ) : ModuleCore( UUID )
 
     this->SetOnCommand( "UPDATE_TRANSFORM", [this]( json::value transform )
     {
-        this->updateTransform( transform );
+        this->onUpdateTransform( transform );
     });
 }
 
-void TransformModule::updateTransform( json::value transform, bool sync = false )
+void TransformModule::onUpdateTransform( json::value transform, bool sync )
 {   
     if( !transform.is_array() ) return;
     auto& liste = transform.as_array();
     if( liste.size() >= 10 )
     {
-        this->translation[0] = liste[0].to_number<float>();
-        this->translation[1] = liste[1].to_number<float>();
-        this->translation[2] = liste[2].to_number<float>();
+        TransformData new_data;
+        new_data.translation[0] = liste[0].to_number<float>();
+        new_data.translation[1] = liste[1].to_number<float>();
+        new_data.translation[2] = liste[2].to_number<float>();
 
-        this->rotation[0] = liste[3].to_number<float>();
-        this->rotation[1] = liste[4].to_number<float>();
-        this->rotation[2] = liste[5].to_number<float>();
-        this->rotation[3] = liste[6].to_number<float>();
+        new_data.rotation[0] = liste[3].to_number<float>();
+        new_data.rotation[1] = liste[4].to_number<float>();
+        new_data.rotation[2] = liste[5].to_number<float>();
+        new_data.rotation[3] = liste[6].to_number<float>();
 
-        this->scale[0] = liste[7].to_number<float>();
-        this->scale[1] = liste[8].to_number<float>();
-        this->scale[2] = liste[9].to_number<float>();
+        new_data.scale[0] = liste[7].to_number<float>();
+        new_data.scale[1] = liste[8].to_number<float>();
+        new_data.scale[2] = liste[9].to_number<float>();
+
+        this->updateTransform( new_data, sync);
     }
-
-    if( sync )
-        this->Output( "UPDATE_TRANSFORM", transform );
 }
 
-tuple<array<float, TRANSLATION_SIZE>, array<float, ROTATION_SIZE>, array<float, SCALE_SIZE>> TransformModule::getTransform()
+void TransformModule::updateTransform( TransformData new_data, bool sync )
+{   
+    this->transform_data = new_data;
+    json::value transform_update = 
+    {
+        this->transform_data.translation[0], 
+        this->transform_data.translation[1],
+        this->transform_data.translation[2],
+        this->transform_data.rotation[0],
+        this->transform_data.rotation[1],
+        this->transform_data.rotation[2],
+        this->transform_data.rotation[3],
+        this->transform_data.scale[0],
+        this->transform_data.scale[1],
+        this->transform_data.scale[2],
+    };
+
+    this->OnChange( "UPDATE_TRANSFORM", transform_update );
+
+    if( sync )
+        this->Output( "UPDATE_TRANSFORM", transform_update );
+}
+
+
+TransformModule::TransformData TransformModule::getTransform()
 {
-    return {translation, rotation, scale};
+    return this->transform_data;
 }
 
 void TransformModule::setState( json::value state )
@@ -53,15 +77,27 @@ void TransformModule::setState( json::value state )
 
     auto it = obj.find( "transform" );
     if( it != obj.end() )
-        this->updateTransform( it->value() );
+        this->onUpdateTransform( it->value() );
 }
 
 json::value TransformModule::getState()
 {
-    json::array liste = {translation[0], translation[1], translation[2], rotation[0], rotation[1], rotation[2], rotation[3], scale[0], scale[1], scale[2]};
+    json::array liste = 
+    {
+        this->transform_data.translation[0], 
+        this->transform_data.translation[1], 
+        this->transform_data.translation[2], 
+        this->transform_data.rotation[0], 
+        this->transform_data.rotation[1], 
+        this->transform_data.rotation[2], 
+        this->transform_data.rotation[3], 
+        this->transform_data.scale[0], 
+        this->transform_data.scale[1], 
+        this->transform_data.scale[2]
+    };
 
     json::object obj;
     obj["transform"] = liste;
 
-    return liste;
+    return obj;
 }
