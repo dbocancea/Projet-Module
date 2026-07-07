53e695613dd41824d05b1adfffbf538c9fc1739c
Shchaslyvyi Ernest <ernipro26@gmail.com>
2026-07-07 13:21:56 +0200

    added webscoket test folder

--- src/ImageModule.hpp
+++ src/ImageModule.hpp
@@ -1,18 +1,18 @@
-#ifndef IMAGE_MODULE_HPP
-#define IMAGE_MODULE_HPP
-#include "TransformModule.hpp"
+// #ifndef IMAGE_MODULE_HPP
+// #define IMAGE_MODULE_HPP
+// #include "TransformModule.hpp"
 
-class ImageModule : public TransformModule
-{
-    protected:
-        json::value image;
-    public:
-        ImageModule();
-        ImageModule(uint128_t UUID);
-        json::value getImage();
-        void setImage(json::value image, bool sync = false);
-        json::value getState();
-        void setState(json::value state);
-};
+// class ImageModule : public TransformModule
+// {
+//     protected:
+//         json::value image;
+//     public:
+//         ImageModule();
+//         ImageModule(uint128_t UUID);
+//         json::value getImage();
+//         void setImage(json::value image, bool sync = false);
+//         json::value getState();
+//         void setState(json::value state);
+// };
 
-#endif
+// #endif
